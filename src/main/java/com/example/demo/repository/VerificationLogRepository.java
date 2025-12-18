1b7623b1fda821f11a9982df8e5660ac5a221ad3
bala-amypo <bala@amypo.in>
2025-12-18 09:55:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationLogRepository.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepository.java
@@ -2,9 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.entity.UserEntity;
+import com.example.demo.entity.VerificationLogEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByEmail(String email);
+public interface VerificationLogRepository extends JpaRepository<VerificationLogEntity, Long> {
 }
