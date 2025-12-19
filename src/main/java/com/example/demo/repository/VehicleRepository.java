150f0811e7bfb4e0c12abf995535c3f030bc9521
bala-amypo <bala@amypo.in>
2025-12-19 08:35:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Vehicle;
+import com.example.demo.model.Vehicle;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
--- src/main/java/com/example/demo/repository/VerificationLogRepository.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.VerificationLog;
+import com.example.demo.model.VerificationLog;
 
 public interface VerificationLogRepository extends JpaRepository<VerificationLog, Long> {
 
