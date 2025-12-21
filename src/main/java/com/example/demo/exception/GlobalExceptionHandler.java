52e78a8bbfe6f812b87eba1a33c37bd329b09e7d
bala-amypo <bala@amypo.in>
2025-12-21 13:35:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(VehicleNotFoundException.class)
+    public String handleVehicleNotFound(VehicleNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(DuplicateVinException.class)
+    public String handleDuplicateVin(DuplicateVinException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidationErrors(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult()
+                 .getFieldError()
+                 .getDefaultMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleGeneralException(Exception ex) {
+        return ex.getMessage();
+    }
+}
