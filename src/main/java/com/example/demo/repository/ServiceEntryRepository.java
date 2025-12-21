78d69e7e0a61bb90783abb3647b946ef3a5c4182
bala-amypo <bala@amypo.in>
2025-12-21 15:54:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServiceEntryRepository.java
+++ src/main/java/com/example/demo/repository/ServiceEntryRepository.java
@@ -1,12 +1,12 @@
-package com.example.demo.repository;
-
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.example.demo.model.ServiceEntry;
 
+@Repository
 public interface ServiceEntryRepository extends JpaRepository<ServiceEntry, Long> {
 
     List<ServiceEntry> findByVehicleId(Long vehicleId);
 
     List<ServiceEntry> findByGarageId(Long garageId);
+    ServiceEntry findTopByVehicleIdOrderByServiceDateDesc(Long vehicleId);
 }
