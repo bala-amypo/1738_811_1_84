86dd8efc280a1c414f5c2ae2d6a695323ef09ddd
bala-amypo <bala@amypo.in>
2025-12-21 14:54:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/ServiceEntryException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class ServiceEntryException extends RuntimeException {
+    public ServiceEntryException(String message) {
+        super(message);
+    }
+}
