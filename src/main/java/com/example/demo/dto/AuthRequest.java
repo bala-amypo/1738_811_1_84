5ecd5884acde2f9121123bd924847275a3114e4f
bala-amypo <bala@amypo.in>
2025-12-23 14:38:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+public class AuthRequest {
+
+    private String email;
+
+    private String password;
+
+    public AuthRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,50 +1,20 @@
 package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import com.example.demo.exception.VehicleNotFoundException;
-import com.example.demo.exception.DuplicateVinException;
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.http.ResponseEntity;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(VehicleNotFoundException.class)
-    public String handleVehicleNotFound(VehicleNotFoundException ex) {
-        return ex.getMessage();
-    }
-
-    @ExceptionHandler(DuplicateVinException.class)
-    public String handleDuplicateVin(DuplicateVinException ex) {
-        return ex.getMessage();
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleValidationErrors(MethodArgumentNotValidException ex) {
-        return ex.getBindingResult()
-                 .getFieldError()
-                 .getDefaultMessage();
-    }
-
-    @ExceptionHandler(Exception.class)
-    public String handleGeneralException(Exception ex) {
-        return ex.getMessage();
-    }
-     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleNotFound(EntityNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-   
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
-
-
-
