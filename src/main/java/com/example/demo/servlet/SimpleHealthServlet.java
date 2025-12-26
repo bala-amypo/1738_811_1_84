9c03f376719e035b2f5629ee481cd76beb97e4f9
bala-amypo <bala@amypo.in>
2025-12-26 16:16:28 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleHealthServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHealthServlet.java
@@ -1,18 +1,17 @@
 package com.example.demo.servlet;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/health")
 public class SimpleHealthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setStatus(200);
         resp.setContentType("text/plain");
-        PrintWriter writer = resp.getWriter();
-        writer.write("OK");
-        writer.flush();
-    }
-}
+        PrintWriter writer = resp
